af6d58093bdda8be37eeff2609d994d542703765
Nick-Ni-cpu <18477@jhc.school.nzjhc.school.nz>
2020-08-19 19:54:15 +0800

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors','On'); //change to 'off' when live
+
+define ('DB_NAME','games');
+define ('DB_USERNAME','root');
+define ('DB_PASSWORD','usbw');
+define ('DB_HOST','localhost');
+
+?>
