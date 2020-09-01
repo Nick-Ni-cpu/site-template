d2e2efc7d97d52097ac9f3578f2669322546ceba
Nick-Ni-cpu <18477@jhc.school.nzjhc.school.nz>
2020-09-01 14:16:10 +0800

    Advanced Search part 2
    dropdown list

--- bottombit.php
+++ bottombit.php
@@ -32,18 +32,36 @@
 					<input class="adv" type="text" name="dev_name" size="30" value="" 
 					placeholder="Developer..." />
 					
-				<input class="submit advanced-button" type='submit' name='advanced' value="Search &nbsp; &#xf002;" />
 				
 				<!-- Genre Dropdown-->
 				
-				<select name='genre'>
+				<select name='genre' class="search adv">
+				
+				<option value='' disabled selected>Genre..</option>
+				
 				<!-- get options from database -->
 					<?php 
+						$genre_sql="SELECT * FROM `genre` ORDER BY `genre`.`Genre` ASC ";
+						$genre_query=mysqli_query($dbconnect, $genre_sql);
+						$genre_rs=mysqli_fetch_assoc($genre_query);
+					
+						do{
+							?>
+							
+							<option value="<?php echo $genre_rs['Genre'] ?>"><?php echo $genre_rs['Genre'] ?></option>
+							
+							<?php
+						} // end genre do loop
+					
+						while($genre_rs=mysqli_fetch_assoc($genre_query))
+					
 					
 					?>
 					
 				</select>
-				
+					
+					<input class="submit advanced-button" type='submit' name='advanced' value="Search &nbsp; &#xf002;" />
+
 				</form>
 				
 				</div> <!-- / advanced frame -->
