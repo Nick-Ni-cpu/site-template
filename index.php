76024b107af495e8c6f918546d9041376e5eff13
Nick-Ni-cpu <18477@jhc.school.nzjhc.school.nz>
2020-09-01 11:05:11 +0800

    Index-acknowledgement

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 				<h2>Welcome</h2>
 				
 				<p>
-					This database includes some of the most popular apps available for mobile devices. The <a href='https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games/version/1'>original dataset</a> can be found at the preceding link.
+					This database includes some of the most popular apps available for mobile devices.
 				</p>
+				<h2>Acknowledgements & Sources</h2>
+				<ul>
+				<li><a href='https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games/version/1'>original "17K Mobile Strategy Games" dataset</a></li>
+				<li>The <a href='https://codepen.io/Bluetidepro/pen/GkpEa'>percentage based star rating</a> code is a the preceding link. It was created by </li>
+				</ul>
 			</div> <!-- / main -->
 			
 <?php include("bottombit.php") ?>
