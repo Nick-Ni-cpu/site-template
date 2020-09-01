a269b4a2442263949816184b2634be5868908460
Nick-Ni-cpu <18477@jhc.school.nzjhc.school.nz>
2020-09-01 15:54:43 +0800

    Advanced search Part 5
    Rating and age

--- advanced.php
+++ advanced.php
@@ -4,7 +4,17 @@
 	$developer = mysqli_real_escape_string($dbconnect, $_POST['dev_name']);
 	$genre = mysqli_real_escape_string($dbconnect, $_POST['genre']);
 	$cost = mysqli_real_escape_string($dbconnect, $_POST['cost']);
- 
+	
+	// Cost code (to handle when cost is not specified...)
+	if ($cost == ''){
+		$cost_op = '>=';
+		$cost = 0;
+	}
+	else{
+		$cost_op = '<=';
+	}
+	
+	// In App Purchase...
 	if(isset($_POST['in_app'])){
 		$in_app = 0;
 	}
@@ -13,7 +23,41 @@
 		$in_app = 1;
 	}
  
+	// Ratings
+	$rating_more_less = mysqli_real_escape_string($dbconnect,$_POST['rate_more_less']);
+	$rating = mysqli_real_escape_string($dbconnect,$_POST['rating']);
  
+	if($rating_more_less == "at least")
+	{
+		$rate_op = ">=";
+	}
+	elseif($rating_more_less == "at most")
+	{
+		$rate_op = "<=";
+	}
+	else
+	{
+		$rate_op = ">=";
+		$rating = 0;
+	} // end rating if / elseif / else
+ 
+ 	// Age...
+	$age_more_less = mysqli_real_escape_string($dbconnect,$_POST['age_more_less']);
+	$age = mysqli_real_escape_string($dbconnect,$_POST['age']);
+ 
+	if($age_more_less == "at least")
+	{
+		$age_op = ">=";
+	}
+	elseif($age_more_less == "at most")
+	{
+		$age_op = "<=";
+	}
+	else
+	{
+		$age_op = ">=";
+		$age = 0;
+	} // end age if / elseif / else
  
  
     $find_sql = "SELECT * FROM `game_details` 
@@ -22,8 +66,10 @@
 	WHERE `Name` LIKE '%$app_name%' 
 	AND `DevName` LIKE '%$developer%'
 	AND `Genre` LIKE '%$genre%'
-	AND `Price` <= '%$cost%'
+	AND `Price` $cost_op '%$cost%'
 	AND (`In App` = $in_app OR `In App` = 0)
+	AND `User Rating` $rate_op $rating
+	AND `Age` $age_op $age
 	";
 	$find_query = mysqli_query($dbconnect, $find_sql);
 	$find_rs = mysqli_fetch_assoc($find_query);
