bf86cceea6f8c8961039ecc877fcf4f4dfbec7d1
Nick-Ni-cpu <18477@jhc.school.nzjhc.school.nz>
2020-09-01 11:29:06 +0800

    Basic Search part 1
    No magnify glass

--- bottombit.php
+++ bottombit.php
@@ -2,13 +2,17 @@
 				<h2>Add an App |
 				<a class='side' href='showall.php'>Show All</a></h2>
 				
-				<p>
-					This is where the search area goes
-				</p>
-			</div> <!-- / side -->
+				<form class="searchform" method="post" action="name_dev.php" enctype="mutipart/form-data">
+				
+					<input class="search" type="text" name="dev-name" size="30" value="" placeholder="App Nam / Developer Name..." required />
+					
+					<input class="submit" type="submit" name="find_game_name" value="&#xf002;" />
+					
+				</form>
+			</div> <!-- / side bar -->
 			
 			<div class="box footer">
-				<p>Mrs Sarah Greeff 2020</p>
+				<p>CC GTT 20XX</p>
 			</div> <!-- / footer -->
 			
 		</div> <!-- / wrapper -->
